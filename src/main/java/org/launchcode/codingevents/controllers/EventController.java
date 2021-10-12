9ea1e75215b8184652e0251532a8a2da22a4eb8c
Minnie Washington <minniewashington@Minnies-MacBook-Pro.local>
2021-10-12 15:33:08 -0500

    finish form2

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,10 +2,7 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,16 +11,12 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-//    private static List<String> events = new ArrayList<>();
+    private static List<String> events = new ArrayList<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
 //        model.addAttribute("title", "All Events");
-        List<String> events = new ArrayList<>();
-        events.add("Code with pride");
-        events.add("Strange Loop");
-        events.add("Apple picking");
-        model.addAttribute("events", events);
+        model.addAttribute("events",events);
         return "events/index";
     }
 
@@ -32,16 +25,13 @@ public class EventController {
     public String renderCreateEventForms(){
         return "events/create";
     }
-//    @GetMapping("create")
-//    public String displayCreateEventForm(Model model) {
-//        model.addAttribute("title", "Create Event");
-//        return "events/create";
-//    }
-//
-//    @PostMapping("create")
-//    public String processCreateEventForm(@RequestParam String eventName) {
-//        events.add(eventName);
-//        return "redirect:";
-//    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
+    }
+
 
 }
